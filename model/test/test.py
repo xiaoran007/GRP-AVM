f7a54431ae290615d3bd71bf410269a3caf48284
xiaoran <xiaoran2495897824@163.com>
2024-02-11 21:26:31 +0800

    update: test.py

--- model/test/test.py
+++ model/test/test.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import numpy as np
 from model import Predictor
 
 
 df = pd.read_csv("./data/X_Full_test.csv")
 print(df.columns.tolist())
 predictor = Predictor.Predictor(X=df, model_sel='RF', lofi=False, lang=False)
-print(predictor.Predict())
+predicted_dict = predictor.Predict()
+if predicted_dict['status'] == 0:
+    values = predicted_dict['values']
+    y_df = pd.read_csv("./data/y_Full_test.csv")["price"].to_numpy()
+    for i in range(len(y_df)):
+        print(f'Predicted: {values[i]}, Actual: {y_df[i]}, Err: {np.abs(values[i] - y_df[i]): .1f}, Percent: {(np.abs(values[i] - y_df[i])) / y_df[i] * 100:.1f}')
+
+# print(predictor.Predict())
