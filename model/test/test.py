b19d9b356704ea8826d0c0b4900f181f79e99a17
xiaoran <xiaoran2495897824@163.com>
2024-02-13 20:44:18 +0800

    update: lofi test

--- model/test/test.py
+++ model/test/test.py
@@ -3,14 +3,43 @@ import numpy as np
 from model import Predictor
 
 
-df = pd.read_csv("./data/X_Full_test.csv")
-print(df.columns.tolist())
-predictor = Predictor.Predictor(X=df, model_sel='RF', lofi=False, lang=False)
-predicted_dict = predictor.Predict()
-if predicted_dict['status'] == 0:
-    values = predicted_dict['values']
-    y_df = pd.read_csv("./data/y_Full_test.csv")["price"].to_numpy()
-    for i in range(len(y_df)):
-        print(f'Predicted: {values[i]}, Actual: {y_df[i]}, Err: {np.abs(values[i] - y_df[i]): .1f}, Percent: {(np.abs(values[i] - y_df[i])) / y_df[i] * 100:.1f}')
+def FULL_TEST():
+    df = pd.read_csv("./data/X_Full_test.csv")
+    print(df.columns.tolist())
+    predictor = Predictor.Predictor(X=df, model_sel='RF', lofi=False, lang=False)
+    predicted_dict = predictor.Predict()
+    High_err = list()
+    if predicted_dict['status'] == 0:
+        values = predicted_dict['values']
+        y_df = pd.read_csv("./data/y_Full_test.csv")["price"].to_numpy()
+        for i in range(len(y_df)):
+            print(f'Predicted: {values[i]}, Actual: {y_df[i]}, Err: {np.abs(values[i] - y_df[i]): .1f}, Percent: {(np.abs(values[i] - y_df[i])) / y_df[i] * 100:.1f}')
+            if (np.abs(values[i] - y_df[i])) / y_df[i] * 100 > 20.05:
+                High_err.append(i)
 
-# print(predictor.Predict())
+        print(len(y_df))
+        print(len(High_err))
+    print(High_err)
+
+
+def LOFI_TEST():
+    df = pd.read_csv("./data/X_Easy_test.csv")
+    print(df.columns.tolist())
+    predictor = Predictor.Predictor(X=df, model_sel='RF', lofi=True, lang=False)
+    predicted_dict = predictor.Predict()
+    High_err = list()
+    if predicted_dict['status'] == 0:
+        values = predicted_dict['values']
+        y_df = pd.read_csv("./data/y_Easy_test.csv")["price"].to_numpy()
+        for i in range(len(y_df)):
+            print(
+                f'Predicted: {values[i]}, Actual: {y_df[i]}, Err: {np.abs(values[i] - y_df[i]): .1f}, Percent: {(np.abs(values[i] - y_df[i])) / y_df[i] * 100:.1f}')
+            if (np.abs(values[i] - y_df[i])) / y_df[i] * 100 > 20.05:
+                High_err.append(i)
+
+        print(len(y_df))
+        print(len(High_err))
+    print(High_err)
+
+
+FULL_TEST()
