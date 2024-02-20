8a36264682f8aa0139137c36461ce0f1960f8810
xiaoran <xiaoran2495897824@163.com>
2024-02-20 16:26:23 +0800

    update: change Data.py and add test

--- Datasets/Data.py
+++ Datasets/Data.py
@@ -57,13 +57,16 @@ class DataLoader(object):
         return X_train, y_train, X_val, y_val, X_test, y_test
 
     @staticmethod
-    def Load(method="keep", normal="MinMax", lofi=False, high=2e6, low=0):
+    def Load(flag=False, method="keep", normal="MinMax", full=True, lofi=False, high=2e6, low=0):
         """
 
         Args:
+            if flag is set, ignore other parameters.
+            flag: full or lofi load.
             high: remove
             low: remove
             lofi: drop rows?
+            full: set False to use subset features. ['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'building_age', 'lat', 'long']
             normal: "MinMax", "Normal", "box-cox", "yeo-johnson" or "None"
             method: "keep" will trans "date" to "year" and "month", "drop" will drop "date"
 
@@ -73,6 +76,29 @@ class DataLoader(object):
         """
         url = 'kc_house_data.csv'
         df = pd.read_csv(url, delimiter=',')
+        if flag is True:
+            if full is True:
+                df['date'] = pd.to_datetime(df['date'])
+                df['year'] = df['date'].apply(lambda date: date.year)
+                df['month'] = df['date'].apply(lambda date: date.month)
+                df['building_age'] = df['year'] - df['yr_built']
+                df['renovated_year'] = DataLoader.Trans(year_renovated=df['yr_renovated'].tolist(), year=df['year'].tolist(), building_age=df['building_age'].tolist())
+                X = df[['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'floors', 'waterfront', 'view', 'condition',
+                        'grade',
+                        'sqft_above', 'sqft_basement', 'building_age', 'renovated_year', 'lat', 'long', 'sqft_living15',
+                        'sqft_lot15', 'year', 'month']]
+                y = df['price']
+                return X, y
+            else:
+                df['date'] = pd.to_datetime(df['date'])
+                df['year'] = df['date'].apply(lambda date: date.year)
+                df['month'] = df['date'].apply(lambda date: date.month)
+                df['building_age'] = df['year'] - df['yr_built']
+                df['renovated_year'] = DataLoader.Trans(year_renovated=df['yr_renovated'].tolist(), year=df['year'].tolist(), building_age=df['building_age'].tolist())
+                X = df[['bedrooms', 'bathrooms', 'sqft_living', 'sqft_lot', 'building_age', 'lat', 'long']]
+                y = df['price']
+                return X, y
+        # if flag is unset:
         if lofi:
             df = df.drop(df[df.price >= high].index)
             df = df.drop(df[df.price <= low].index)
@@ -96,6 +122,16 @@ class DataLoader(object):
             print("normal is None")
             pass
         return X, y
+
+    @staticmethod
+    def Trans(year_renovated, year, building_age):
+        return_list = list()
+        for i in range(len(year_renovated)):
+            if year_renovated[i] == 0:
+                return_list.append(building_age[i])
+            else:
+                return_list.append(year[i] - year_renovated[i])
+        return return_list
 
 
 class Preprocessing(object):
@@ -121,7 +157,7 @@ class Preprocessing(object):
 def Default(cwd):
     os.chdir(os.path.dirname(__file__))
     print(f"set dir: {os.getcwd()}")
-    X, y = DataLoader().Load(method="keep", normal="MinMax", lofi=False)
+    X, y = DataLoader().Load(flag=True, full=True)
     X_train, y_train, X_test, y_test = Preprocessing(X, y, random_state=62).DoNothing()
     os.chdir(cwd)
     print(f"set dir back: {cwd}")
--- model/test/test.py
+++ model/test/test.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from model import Predictor
+from Datasets.Data import Default
+import os
 
 
 def FULL_TEST():
@@ -42,4 +44,23 @@ def LOFI_TEST():
     print(High_err)
 
 
-FULL_TEST()
+def TEST():
+    X_train, y_train, X_test, y_test = Default(os.path.dirname(__file__))
+    predictor = Predictor.Predictor(X=X_train, model_sel='RF', lofi=False, lang=False)
+    predicted_dict = predictor.Predict()
+    High_err = list()
+    if predicted_dict['status'] == 0:
+        values = predicted_dict['values']
+        y_df = y_train.to_numpy()
+        for i in range(len(y_df)):
+            print(
+                f'Predicted: {values[i]}, Actual: {y_df[i]}, Err: {np.abs(values[i] - y_df[i]): .1f}, Percent: {(np.abs(values[i] - y_df[i])) / y_df[i] * 100:.1f}')
+            if (np.abs(values[i] - y_df[i])) / y_df[i] * 100 > 20.05:
+                High_err.append(i)
+
+        print(len(y_df))
+        print(len(High_err))
+    print(High_err)
+
+
+TEST()
