2cf6054efbc0a8023de3262b5465cde24cc3c060
xiaoran <xiaoran2495897824@163.com>
2024-02-20 22:16:44 +0800

    update: test

--- test/test.py
+++ test/test.py
@@ -7,9 +7,13 @@ import os
 def TEST():
     X_train, y_train, X_test, y_test = Default(os.path.dirname(__file__))
     X_train_numpy = X_train.to_numpy()
-    predictor = Predictor(X_train_numpy[0],  model_sel='RF', full=True, lang=False, cwd=os.path.dirname(__file__))
+    sel = X_train_numpy[0]
+    predictor = Predictor(sel,  model_sel='RF', full=True, lang=False, cwd=os.path.dirname(__file__))
     pred_price = predictor.Predict()
     print(pred_price)
+    descriptor = Descriptor(sel, pred_price['values'][0], full=True, cwd=os.path.dirname(__file__))
+    text = descriptor.GetDescription()
+    print(text)
 
 
 TEST()
