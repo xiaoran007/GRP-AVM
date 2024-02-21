8d231e194ce70f5033e3dc347f7a4ea590cbcf79
xiaoran <xiaoran2495897824@163.com>
2024-02-21 23:19:26 +0800

    update: pro mode ui

--- Interface/app.py
+++ Interface/app.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+import util
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def pro_mode():
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        return "undefined"
 
+
+@app.route('/result', methods=['POST'])
+def pro_result():
+    if request.method == 'POST':
+        print(request.form)
+        ar = util.data_preprocessing(request.form, full=True)
+        print(ar)
+        pred_price, text = util.backend(ar, full=True)
+        return f"OK\nPrice: {pred_price}\nText: {text}"
 
 if __name__ == '__main__':
     app.run()
