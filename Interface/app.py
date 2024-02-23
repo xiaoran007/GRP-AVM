f30693d09d4c080763e6f4b5477e89d4e7e630c3
xiaoran <xiaoran2495897824@163.com>
2024-02-23 20:50:12 +0800

    update: add normalModeForm.html to app

--- Interface/app.py
+++ Interface/app.py
@@ -48,5 +48,13 @@ def getstarted():
         return "undefined"
 
 
+@app.route('/normal_mode_form', methods=['GET', 'POST'])
+def normal_mode_form():
+    if request.method == 'GET':
+        return render_template('normalModeForm.html')
+    elif request.method == 'POST':
+        return "undefined"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
