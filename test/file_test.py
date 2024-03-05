bcf44319729b8b02f945f9525bcdb90621f87f58
xiaoran <xiaoran2495897824@163.com>
2024-03-05 22:11:19 +0800

    add: file_test.py

---
+++ test/file_test.py
@@ -0,0 +1,18 @@
+from Datasets.Data import Default, Default_Easy
+import pandas as pd
+import os
+
+def genExample(full=True):
+    if full:
+        X_train, y_train, X_test, y_test = Default(os.path.dirname(__file__))
+        file_name = './Full_example.csv'
+    else:
+        X_train, y_train, X_test, y_test = Default_Easy(os.path.dirname(__file__))
+        file_name = './Easy_example.csv'
+    exampleDF = X_test.head(10)
+    print(exampleDF)
+    exampleDF.to_csv(file_name, index=False)
+
+
+genExample(full=True)
+genExample(full=False)
