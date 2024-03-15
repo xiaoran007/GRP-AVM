5068fd1c8ff9df139374f26bce10383603f1aefe
xiaoran <xiaoran2495897824@163.com>
2024-03-15 17:29:25 +0800

    update: init predictor test

---
+++ test/test_Predictor.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+def test_predict():
+    assert True
+
+
+def test_predict_by_x():
+    assert False
+
+
+def test_numpy2df():
+    assert False
+
+
+def test_load_model():
+    assert False
+
+
+def test_check_model():
+    assert False
