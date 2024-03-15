516ddc662711cd46b913d59824f3f967dd9c984b
xiaoran <xiaoran2495897824@163.com>
2024-03-15 17:16:40 +0800

    update: add resources

--- test/file_test.py
+++ test/file_test.py
@@ -5,10 +5,10 @@ import os
 def genExample(full=True):
     if full:
         X_train, y_train, X_test, y_test = Default(os.path.dirname(__file__))
-        file_name = './Full_example.csv'
+        file_name = 'resources/Full_example.csv'
     else:
         X_train, y_train, X_test, y_test = Default_Easy(os.path.dirname(__file__))
-        file_name = './Easy_example.csv'
+        file_name = 'resources/Easy_example.csv'
     exampleDF = X_test.head(10)
     print(exampleDF)
     exampleDF.to_csv(file_name, index=False)
