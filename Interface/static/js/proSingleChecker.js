838a44ebd4e67b9ca15a10e420a825fee74096ab
xiaoran <xiaoran2495897824@163.com>
2024-03-16 17:33:30 +0800

    update: add zipcode js

--- Interface/static/js/proSingleChecker.js
+++ Interface/static/js/proSingleChecker.js
@@ -1,36 +1,44 @@
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("proSingle");
-    const quantityInput = document.getElementById("zipcode");
+    const zipcodeInput = document.getElementById("zipcode");
     const modal = new bootstrap.Modal(document.getElementById('myModal'));
     const closeBtn = document.getElementById("closeBtn");
+    const errMsg = document.getElementById("errMsg");
 
-    // Function to validate the input
-    function validateInput() {
-        const quantity = parseFloat(quantityInput.value);
-        if (isNaN(quantity) || quantity > 10) {
-            displayCustomAlert();
+    function validateZipcode(inside) {
+        const zipcode = Number(zipcodeInput.value);
+        console.log(zipcode)
+        if (isNaN(zipcode) || (!Number.isInteger(zipcode)) || zipcode < 0) {
+            if (inside) {
+                displayCustomAlert("Please enter a valid zipcode.");
+            }
             return false;
-        } else {
+        }
+        else {
             return true;
         }
     }
 
+    zipcodeInput.addEventListener('blur', function() {
+        validateZipcode(true);
+    });
+
     // Function to display custom alert
     function displayCustomAlert(err_msg) {
-        var errMsg = document.getElementById("errMsg");
         errMsg.textContent = err_msg;
-        // var modal = new bootstrap.Modal(document.getElementById('ModalCenter'));
         modal.toggle();
     }
 
     function hideCustomAlert() {
-        console.log('sss')
-        // var modal = new bootstrap.Modal(document.getElementById('ModalCenter'));
         modal.hide();
     }
 
-    quantityInput.addEventListener('blur', validateInput);
-
     closeBtn.addEventListener('click', hideCustomAlert);
+
+    form.addEventListener("submit", function(event) {
+        if (!validateInput()) {
+            event.preventDefault(); // Prevent form submission if input is invalid
+        }
+    });
 
 });
