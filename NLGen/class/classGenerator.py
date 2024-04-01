0271432f48a1196757b321bcc35dc3f6328a14b7
xiaoran <xiaoran2495897824@163.com>
2024-04-01 14:25:13 +0800

    update: add copyright to NLGen

--- NLGen/class/classGenerator.py
+++ NLGen/class/classGenerator.py
@@ -1,3 +1,14 @@
+"""
+Copyright (c) 2024 Tangtangfang Fang
+
+All rights reserved.
+
+This file is part of AVM-GRP.
+
+AVM-GRP is distributed under the GPLv3 License.
+See the LICENSE file at the top level of the distribution for details.
+"""
+
 import os
 
 from Datasets.Data import DataLoader, Preprocessing, Default, Default_Easy
