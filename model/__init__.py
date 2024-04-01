23ab1ab33acaf8c071b2a5f04a352ac4080a8ef6
xiaoran <xiaoran2495897824@163.com>
2024-04-01 14:24:31 +0800

    update: add copyright to model

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,11 @@
+"""
+Copyright (c) 2024 Tangtangfang Fang
 
+All rights reserved.
+
+This file is part of AVM-GRP.
+
+AVM-GRP is distributed under the GPLv3 License.
+See the LICENSE file at the top level of the distribution for details.
+"""
 
