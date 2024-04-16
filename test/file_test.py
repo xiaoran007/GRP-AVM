fbe9078d45494bc8623cbc0e8e73584dc7425cb5
xiaoran <xiaoran2495897824@163.com>
2024-04-16 20:26:49 +0800

    update: add files for open day

--- test/file_test.py
+++ test/file_test.py
@@ -12,6 +12,7 @@ See the LICENSE file at the top level of the distribution for details.
 import os
 
 from Datasets.Data import Default, Default_Easy
+from model.Predictor import CpPredictor
 import pandas as pd
 
 """
@@ -20,16 +21,42 @@ import pandas as pd
 
 
 def genExample(full=True):
+    cp_predictor = CpPredictor(X=None, cwd=os.path.dirname(__file__), full=full, alpha=0.2)
     if full:
         X_train, y_train, X_test, y_test = Default(os.path.dirname(__file__))
+        file_name_no_price = 'resources/Full_example_OpenDay_NoPrice.csv'
         file_name = 'resources/Full_example_OpenDay.csv'
     else:
         X_train, y_train, X_test, y_test = Default_Easy(os.path.dirname(__file__))
+        file_name_no_price = 'resources/Easy_example_OpenDay_NoPrice.csv'
         file_name = 'resources/Easy_example_OpenDay.csv'
-    exampleDF = X_test.head(10)
-    exampleDF = pd.concat([exampleDF, y_test.head(10)], axis=1)
+    X = X_train
+    X_numpy = X.to_numpy()
+    y = y_train
+    t_numpy = y.to_numpy()
+    res = list()
+    count = 0
+    for i in range(len(X_numpy)):
+        pred_price = cp_predictor.PredictByX(X_numpy[i], ALPHA=0.2)
+        low = int(round(pred_price['values_range'][0], -2))
+        high = int(round(pred_price['values_range'][1], -2))
+        y_pred = pred_price['values'][0]
+        error = abs((y_pred - t_numpy[i]) / t_numpy[i])
+        cp_err = (high - ((high + low) / 2)) / ((high + low) / 2)
+        if cp_err <= 0.15:
+            print(f"find in {i}, cp_err: {cp_err}")
+            res.append(i)
+            count += 1
+        if count == 10:
+            break
+
+    exampleDF_NoPrice = X.iloc[res]
+    print(exampleDF_NoPrice)
+    exampleDF_NoPrice.to_csv(file_name_no_price, index=False)
+    exampleDF = pd.concat([exampleDF_NoPrice, y.iloc[res]], axis=1)
     print(exampleDF)
     exampleDF.to_csv(file_name, index=False)
+
 
 
 genExample(full=True)
