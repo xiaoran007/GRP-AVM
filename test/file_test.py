2ea585f7b0a15f8e27fa209609fcef8c8b5f86b0
xiaoran <xiaoran2495897824@163.com>
2024-04-16 17:43:01 +0800

    update: add files for open day

--- test/file_test.py
+++ test/file_test.py
@@ -12,6 +12,7 @@ See the LICENSE file at the top level of the distribution for details.
 import os
 
 from Datasets.Data import Default, Default_Easy
+import pandas as pd
 
 """
     This file is not final test file.
@@ -21,11 +22,12 @@ from Datasets.Data import Default, Default_Easy
 def genExample(full=True):
     if full:
         X_train, y_train, X_test, y_test = Default(os.path.dirname(__file__))
-        file_name = 'resources/Full_example.csv'
+        file_name = 'resources/Full_example_OpenDay.csv'
     else:
         X_train, y_train, X_test, y_test = Default_Easy(os.path.dirname(__file__))
-        file_name = 'resources/Easy_example.csv'
+        file_name = 'resources/Easy_example_OpenDay.csv'
     exampleDF = X_test.head(10)
+    exampleDF = pd.concat([exampleDF, y_test.head(10)], axis=1)
     print(exampleDF)
     exampleDF.to_csv(file_name, index=False)
 
